139ae49c59310eddae4d2bd8aae48c0ce872286d
mbasyar <muhammadkhoirulb@gmail.com>
2023-05-08 19:04:30 +0700

    add log

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./signup.module.css";
+import {Link, useNavigate} from "react-router-dom";
+import classes from './signup.module.css';
+import img from '../../assets/womaneating.jpg'
+import { useDispatch } from "react-redux";
 
 const Signup = () => {
+
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  
+  const handleSignup = async(e) => {
+    e.prevenDefault()
+    try {
+      const res= await fetch('http://localhost:5000/auth/register', {
+        headers : {
+          "Content-Type": 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({userName, email, password})
+        
+      })
+      const data = await res.json()
+      dispatchEvent(register(data))
+      navigate('/')
+    }
+    catch (error)
+    setError(true)
+    setTimeout(() => {
+      setError(false)
+    }, 3000);
+  }
+
   return (
   <div className={classes.signUpContainer}>
     <div className={classes.signUpWrapper}>
